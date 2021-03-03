83b132f2835e8456f435d1f3bfaa6bbdda34e52a
qStivi <stephanglaue@yahoo.de>
2021-03-03 14:15:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CSharp_VS_Java
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            for (var i = 0; i < 10000; i++) Console.WriteLine("HelloWorld!");
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
